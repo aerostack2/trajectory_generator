2591d7f20936a2ad6dbcff9e66fda8694c4b9928
perezsaura-david <perezsaura.david@gmail.com>
2022-02-25 18:15:15 +0100

    Add log-level to launch file

--- launch/as2_trajectory_generator_launch.py
+++ launch/as2_trajectory_generator_launch.py
@@ -1,17 +1,22 @@
+from ast import arguments
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
+
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value='drone0'),
+        DeclareLaunchArgument('log_level', default_value='error'),
         Node(
             package='as2_trajectory_generator',
             executable='as2_trajectory_generator_node',
             name='as2_trajectory_generator',
             namespace=LaunchConfiguration('drone_id'),
             output='screen',
+            arguments=['--ros-args', '--log-level',
+                       LaunchConfiguration('log_level')],
             emulate_tty=True
         )
     ])
