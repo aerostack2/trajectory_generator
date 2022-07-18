a11e3a7ba9f96b20e64f4ab5fb0efb87087b5f98
Miguel <miferco97@gmail.com>
2022-07-18 13:51:26 +0200

    v0.2.0

--- launch/trajectory_generator_launch.py
+++ launch/trajectory_generator_launch.py
@@ -3,7 +3,6 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, EnvironmentVariable
 
-
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
